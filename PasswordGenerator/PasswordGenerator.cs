98e37279305b8a9ea00c262dfac6bad76ae9ee54
Grigory <68439684+Sqr3m@users.noreply.github.com>
2022-09-12 20:50:32 +0300

    Add project files.

---
+++ PasswordGenerator/PasswordGenerator.cs
@@ -0,0 +1,27 @@
+﻿namespace PasswordGenerator
+{
+    public class PasswordGenerator
+    {
+        public static string Generate(long passwordLength)
+        {
+            string password = "";
+            string letters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*_+";
+            
+            Random random = new();
+
+            if (passwordLength >= 8)
+            {
+                for (int i = 0; i < passwordLength; i++)
+                {
+                password += $"{letters[random.Next(letters.Length)]}";
+                }
+                
+                return $"Password ({passwordLength} symbols):\n{password}\n\n";
+            }
+            else
+            {
+                return "Password must not be null.";
+            }
+        }
+    }
+}
---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace PasswordGenerator
+{
+    class Program
+    {
+        static void Main()
+        {
+            long length;
+            string? action;
+
+            while (true)
+            {
+                Console.WriteLine("Actions\n1 - generate password\n0 - quit");
+                Console.Write("Action: ");
+                action =  Console.ReadLine();
+
+                switch (action)
+                {
+                    case "1":
+                        Console.Write("Password length: ");
+                        length = Convert.ToInt64(Console.ReadLine());
+                        Console.WriteLine(PasswordGenerator.Generate((int)length));
+
+                        continue;
+
+                    case "0":
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Please enter an action.");
+                        break;
+                    }
+            }
+        }
+    }
+}
